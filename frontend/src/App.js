f5fa4c4c9aca2d09473d22475232cf29f7c2be94
Volodymyr Myshko <vmyshko@equityeffect.com>
2021-05-31 21:34:51 +0300

    basic img increase/decrease/reset functionality impl

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const defaultImgCount = 1;
+  const maxImgCount = 10;
+
+  const [imgCount, setImgCount] = useState(defaultImgCount);
+
+  function increaseImgCount() {
+    if (imgCount >= maxImgCount) return;
+
+    setImgCount(imgCount + 1);
+  }
+  function decreaseImgCount() {
+    if (imgCount <= defaultImgCount) return;
+
+    setImgCount(imgCount - 1);
+  }
+
+  function resetImgCount() {
+    setImgCount(defaultImgCount);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <button onClick={increaseImgCount}>➕</button>
+        <button onClick={decreaseImgCount}>➖</button>
+        <button onClick={resetImgCount}>⏺ reset</button>
       </header>
+
+      {Array(imgCount)
+        .fill(0)
+        .map((_, index) => (
+          // in this case using array index as a key is fine
+          <img key={index} src={logo} className="App-logo" alt="logo" />
+        ))}
     </div>
   );
 }
